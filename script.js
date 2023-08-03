371e23c192c2787a5cd8d00589ad0f28f2a11a05
Giresse86 <giressetuekam@gmail.com>
2023-08-03 09:59:07 +0200

    change content of readme

--- script.js
+++ script.js
@@ -38,12 +38,12 @@ myAlphabetLength();
 
 // Exo 2
 
-let planets = ["Terre", "Venus", "Jupiter", "Mars", "Saturne"];
-console.log(planets);
+// let planets = ["Terre", "Venus", "Jupiter", "Mars", "Saturne"];
+// console.log(planets);
 
-planets.forEach((planet, index) => {
-  console.log(planet, index);
-});
+// planets.forEach((planet, index) => {
+//   console.log(planet, index);
+// });
 
 // Exo 3
 
@@ -68,20 +68,20 @@ console.log(`${furnitures} ${furnitures.length}`);
 
 // Exo 6
 let values1 = ["Apple", 1, false, "2"];
-let filteredvalues1 = values1.filter((value1, index) => {
-  if (values1 !== "number") {
+let newvalues1 = values1.map((value1) => {
+  if (value1 !== "number") {
     return value1;
   }
 });
-console.log(filteredvalues1);
+console.log(newvalues1);
 
 let values2 = [`5`, "Fries", 2, true];
-let removeditems2 = values2.splice(0, 2);
-console.log(values2);
-
-let values3 = ["Mars", "Strawberry", 9];
-let removeditems3 = values3.splice(0, 2);
-console.log(values3);
+let newvalues2 = values2.map((value2) => {
+  if (value2 !== "number") {
+    return value2;
+  }
+});
+console.log(newvalues2);
 
 // Exo 7
 let studentCoursesA = ["Math", "English", "Programming"];
@@ -97,38 +97,3 @@ function isEqual(studentCoursesA, studentCoursesB) {
 let resultat = isEqual(studentCoursesA, studentCoursesB);
 
 console.log(resultat);
-
-
-// Resume du cours sur les variables
-// Variables & types de variables
-
-// let // Variable portée limitée et qui est réassignable.
-// const // Vraible constante à portée limité mais qui n'est pas réassignable.
-
-const lastname = "Ielli"; // Non réassignable.
-let age = 30; // Réassignable.
-
-const firstname = "Lucas"; // Portée globale.
-let javaScriptCoach = "Lucas"; // Portée globale.
-
-const catName = "Lucky"; // Type > String = chaine de caractère = ""; Valeur : Lucky
-console.log(catName);
-let catAge = 4; // Type > Number = nombre = 0; Valeur : 4
-console.log(catAge);
-let vraiOuFaux = true; // Type > Boolean = Booléen = true ou false; Valeur : true
-console.log(vraiOuFaux);
-const planets = ["Uranus", "Venus"]; // Type de la variable > Tableau = Array = [] // Valeurs reprises dans notre tableau sont toutes les deux des Strings.
-console.log(planets); // (2)['Uranus', 'Venus']
-console.log(planets[0]); // Uranus // Faire le choix entre [] de l'indice permet d'afficher la valeur de cet indice.
-
-const voiture = {
-  marque: "Toyota",
-  modele: "Yaris",
-  annee: 1999,
-}; // Type de la variable voiture > Objet = Object = {} // Cet objet possède 3 propriété qui sont : la marque, le modele, l'année. // Les valeurs des deux propriétés marque et modele sont des Strings : Toyota, Yaris. Et l'année est un type Nombre.
-console.log(voiture); // {marque: 'Toyota', modele: 'Yaris', annee: 1999}
-console.log(voiture.marque); // 'Toyota'
-console.log(voiture.modele); // 'Yaris'
-console.log(voiture.annee); // 1999
-
-Envoyer un message dans #general
